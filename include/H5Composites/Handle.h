e8d8ed940a2114b527d940d79131562eb241c54c
Jon burr <Jon-Burr@users.noreply.github.com>
2021-08-11 16:03:18 +0100

    Add handle class for scalar datasets

---
+++ include/H5Composites/Handle.h
@@ -0,0 +1,39 @@
+#ifndef H5COMPOSITES_HANDLE_H
+#define H5COMPOSITES_HANDLE_H
+
+#include "H5Cpp.h"
+#include "H5Composites/DTypes.h"
+
+namespace H5Composites {
+    template <typename T>
+    class Handle
+    {
+    public:
+        Handle(H5::Group& group, const std::string& name, const T& value);
+
+        ~Handle();
+
+        // Explicitly disallow copying
+        Handle(const Handle& other) = delete;
+        Handle(Handle&& other);
+
+        T& operator*() { return m_value; }
+        const T& operator*() const { return m_value; }
+        T* operator->() { return &m_value; }
+        const T* operator->() const { return &m_value; }
+    private:
+        H5::Group& m_targetGroup;
+        std::string m_name;
+        T m_value;
+    }; //> end class Handle<T>
+
+    template <typename T>
+    Handle<T> makeHandle(H5::Group& group, const std::string& name, const T& value)
+    {
+        return Handle<T>(group, name, value);
+    }
+} //> end namespace H5Composites
+
+#include "H5Composites/Handle.icc"
+
+#endif //> !H5COMPOSITES_HANDLE_H
