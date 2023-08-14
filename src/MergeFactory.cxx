2915f417591a666b027792fd587a0ba09a2b0025
Jon burr <Jon-Burr@users.noreply.github.com>
2023-08-14 22:50:47 +0100

    MergeFactory implementation

--- src/MergeFactory.cxx
+++ src/MergeFactory.cxx
@@ -1,1 +1,28 @@
 #include "H5Composites/MergeFactory.hxx"
+
+namespace H5Composites {
+    MergeFactory &MergeFactory::instance() {
+        static MergeFactory factory;
+        return factory;
+    }
+
+    bool MergeFactory::registerRule(TypeRegister::id_t id, rule_t rule) {
+        if (id == TypeRegister::nullID)
+            throw std::invalid_argument("Cannot register using the null ID");
+        if (m_rules.count(id))
+            throw std::invalid_argument("Cannot re-register ID " + std::to_string(id.value));
+        return m_rules.emplace(id, rule).second;
+    }
+
+    MergeFactory::rule_t MergeFactory::retrieve(TypeRegister::id_t id) const {
+        return m_rules.at(id);
+    }
+
+    bool MergeFactory::contains(TypeRegister::id_t id) const { return m_rules.contains(id); }
+
+    H5Buffer MergeFactory::merge(
+            TypeRegister::id_t id, const std::vector<H5BufferConstView> &buffers) const {
+        return retrieve(id)(buffers);
+    }
+
+} // namespace H5Composites
