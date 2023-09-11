889686899a994eb7009dcbc97e3e94e5da8eec25
Jon-Burr <Jon-Burr@users.noreply.github.com>
2023-09-11 10:40:16 +0000

    deploy: 966fbbf10c4f175cfcb9e1b84c7eb0c2ede40465

--- search/all_10.js
+++ search/all_10.js
@@ -3,7 +3,7 @@ var searchData=
   ['tostring_0',['toString',['../classH5Composites_1_1DTypeIterator.html#aa6b53c7f6268cba88b33095f29e11496',1,'H5Composites::DTypeIterator']]],
   ['transfervlenownership_1',['transferVLenOwnership',['../classH5Composites_1_1H5Buffer.html#a8cb4c730a7fb3aa50a63300d808a7a78',1,'H5Composites::H5Buffer']]],
   ['typedreader_2',['typedreader',['../classH5Composites_1_1TypedReader.html',1,'H5Composites::TypedReader&lt; T &gt;'],['../classH5Composites_1_1TypedReader.html#a2d4976d7f2b8955ae268e587709d6a02',1,'H5Composites::TypedReader::TypedReader()']]],
-  ['typedwriter_3',['typedwriter',['../classH5Composites_1_1TypedWriter.html',1,'H5Composites::TypedWriter&lt; T &gt;'],['../classH5Composites_1_1TypedWriter.html#a2d0fec99eeb3fdcaf431fb2a46544e46',1,'H5Composites::TypedWriter::TypedWriter()']]],
+  ['typedwriter_3',['typedwriter',['../classH5Composites_1_1TypedWriter.html',1,'H5Composites::TypedWriter&lt; T &gt;'],['../classH5Composites_1_1TypedWriter.html#a95d0a8bbd4c51353bf604d050939cf3b',1,'H5Composites::TypedWriter::TypedWriter()']]],
   ['typeidtraits_4',['TypeIDTraits',['../structH5Composites_1_1TypeIDTraits.html',1,'H5Composites']]],
   ['typeregister_5',['TypeRegister',['../classH5Composites_1_1TypeRegister.html',1,'H5Composites']]]
 ];
--- search/functions_10.js
+++ search/functions_10.js
@@ -3,5 +3,5 @@ var searchData=
   ['tostring_0',['toString',['../classH5Composites_1_1DTypeIterator.html#aa6b53c7f6268cba88b33095f29e11496',1,'H5Composites::DTypeIterator']]],
   ['transfervlenownership_1',['transferVLenOwnership',['../classH5Composites_1_1H5Buffer.html#a8cb4c730a7fb3aa50a63300d808a7a78',1,'H5Composites::H5Buffer']]],
   ['typedreader_2',['TypedReader',['../classH5Composites_1_1TypedReader.html#a2d4976d7f2b8955ae268e587709d6a02',1,'H5Composites::TypedReader']]],
-  ['typedwriter_3',['TypedWriter',['../classH5Composites_1_1TypedWriter.html#a2d0fec99eeb3fdcaf431fb2a46544e46',1,'H5Composites::TypedWriter']]]
+  ['typedwriter_3',['TypedWriter',['../classH5Composites_1_1TypedWriter.html#a95d0a8bbd4c51353bf604d050939cf3b',1,'H5Composites::TypedWriter']]]
 ];
